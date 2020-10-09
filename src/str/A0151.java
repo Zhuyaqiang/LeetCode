1b61b1ace01a2d172a6d564a0bb730134853f234
Zhuyaqiang <zyq1512099831@gmail.com>
2020-10-09 18:59:17 +0800

    1009

---
+++ src/str/A0151.java
@@ -0,0 +1,97 @@
+package str;
+
+/**
+ * 翻转字符串里的单词
+ * 给定一个字符串，逐个翻转字符串中的每个单词。
+ * 说明：
+ * 无空格字符构成一个 单词 。
+ * 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+ * 如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+ * 示例 1：
+ * 输入："the sky is blue"
+ * 输出："blue is sky the"
+ * 示例 2：
+ * 输入："  hello world!  "
+ * 输出："world! hello"
+ * 解释：输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+ * 示例 3：
+ * 输入："a good   example"
+ * 输出："example good a"
+ * 解释：如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+ * 示例 4：
+ * 输入：s = "  Bob    Loves  Alice   "
+ * 输出："Alice Loves Bob"
+ * 示例 5：
+ * 输入：s = "Alice does not even like bob"
+ * 输出："bob like even not does Alice"
+ * 提示：
+ * 1 <= s.length <= 104
+ * s 包含英文大小写字母、数字和空格 ' '
+ * s 中 至少存在一个 单词
+ * 进阶：
+ * 请尝试使用 O(1) 额外空间复杂度的原地解法。
+ */
+public class A0151 {
+    public static void main(String[] args) {
+        System.out.println("|" + reverseWords2("  hello world  ") + "|");
+    }
+    public static String reverseWords(String s) {
+        String[] sp = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < sp.length; i++) {
+            if (sp[i].length() == 0)
+                continue;
+            sb = sb.insert(0, " " + sp[i]);
+        }
+        return sb.substring(1);
+    }
+    public static String reverseWords2(String s) {
+        // 去除无效空格
+        StringBuilder sb = new StringBuilder();
+        int left = 0, right = s.length() - 1;
+        while (left <= right && s.charAt(left) == ' ')
+            left ++;
+        while (right >= left && s.charAt(right) == ' ')
+            right --;
+        while (left <= right) {
+            char c = s.charAt(left);
+            if (c != ' ')
+                sb.append(c);
+            else {
+                if (sb.charAt(sb.length() - 1) != ' ')
+                    sb.append(c);
+            }
+            left ++;
+        }
+
+        // 反转字符串
+        int len = sb.length();
+        left = 0;
+        right = len-1;
+        while (left < right) {
+            char temp = sb.charAt(left);
+            sb.setCharAt(left, sb.charAt(right));
+            sb.setCharAt(right, temp);
+            left ++;
+            right --;
+        }
+
+        // 反转每个单词
+        left = 0;
+        while (left < len) {
+            right = left;
+            while (right < len && sb.charAt(right) != ' ')
+                right ++;
+            int l = left, r = right - 1;
+            while (l < r) {
+                char temp = sb.charAt(l);
+                sb.setCharAt(l, sb.charAt(r));
+                sb.setCharAt(r, temp);
+                l ++;
+                r --;
+            }
+            left = right + 1;
+        }
+        return sb.toString();
+    }
+}
---
+++ src/str/A0165.java
@@ -0,0 +1,72 @@
+package str;
+
+/**
+ * 比较版本号
+ * 比较两个版本号 version1 和 version2。
+ * 如果 version1 > version2 返回 1，如果 version1 < version2 返回 -1， 除此之外返回 0。
+ * 你可以假设版本字符串非空，并且只包含数字和 . 字符。
+ *  . 字符不代表小数点，而是用于分隔数字序列。
+ * 例如，2.5 不是“两个半”，也不是“差一半到三”，而是第二版中的第五个小版本。
+ * 你可以假设版本号的每一级的默认修订版号为 0。例如，版本号 3.4 的第一级（大版本）和第二级（小版本）修订号分别为 3 和 4。其第三级和第四级修订号均为 0。
+ * 示例 1:
+ * 输入: version1 = "0.1", version2 = "1.1"
+ * 输出: -1
+ * 示例 2:
+ * 输入: version1 = "1.0.1", version2 = "1"
+ * 输出: 1
+ * 示例 3:
+ * 输入: version1 = "7.5.2.4", version2 = "7.5.3"
+ * 输出: -1
+ * 示例 4：
+ * 输入：version1 = "1.01", version2 = "1.001"
+ * 输出：0
+ * 解释：忽略前导零，“01” 和 “001” 表示相同的数字 “1”。
+ * 示例 5：
+ * 输入：version1 = "1.0", version2 = "1.0.0"
+ * 输出：0
+ * 解释：version1 没有第三级修订号，这意味着它的第三级修订号默认为 “0”。
+ */
+public class A0165 {
+    public static void main(String[] args) {
+        System.out.println(compareVersion("0.1", "1.1"));
+        System.out.println(compareVersion("1.0.1", "1"));
+        System.out.println(compareVersion("7.5.2.4", "7.5.3"));
+        System.out.println(compareVersion("1.01", "1.001"));
+        System.out.println(compareVersion("1.0", "1.0.0"));
+    }
+    public static int compareVersion(String version1, String version2) {
+        String[][] split = new String[2][];
+        split[0] = version1.split("\\.");
+        split[1] = version2.split("\\.");
+        int minLen, big;
+        if (split[0].length > split[1].length) {
+            minLen = split[1].length;
+            big = 0;
+        } else {
+            minLen = split[0]. length;
+            big = 1;
+        }
+        for (int i = 0; i < minLen; i++) {
+            int oneVal = getInt(split[0][i]);
+            int twoVal = getInt(split[1][i]);
+            if (oneVal > twoVal)
+                return 1;
+            else if (oneVal < twoVal)
+                return -1;
+        }
+        for (int i = minLen; i < split[big].length; i++) {
+            int val = getInt(split[big][i]);
+            if (val != 0) {
+                return big == 0 ? 1 : -1;
+            }
+        }
+        return 0;
+    }
+    public static int getInt(String s) {
+        int val = 0;
+        for (int one = 0; one < s.length(); one++) {
+            val = val * 10 + (s.charAt(one) - '0');
+        }
+        return val;
+    }
+}
