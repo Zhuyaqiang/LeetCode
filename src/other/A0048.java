15de7f72190f37ffce2bf90724278e46aeaefd3f
Zhuyaqiang <zyq1512099831@gmail.com>
2020-12-19 20:08:49 +0800

    1219

---
+++ src/other/A0048.java
@@ -0,0 +1,84 @@
+package other;
+
+/**
+ * 旋转图像
+ * 给定一个 n × n 的二维矩阵表示一个图像。
+ * 将图像顺时针旋转 90 度。
+ * 说明：
+ * 你必须在原地旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要使用另一个矩阵来旋转图像。
+ * 示例 1:
+ * 给定 matrix =
+ * [
+ *   [1,2,3],
+ *   [4,5,6],
+ *   [7,8,9]
+ * ],
+ *
+ * 9 6 3
+ * 8 5 2
+ * 7 4 1
+ *
+ * 原地旋转输入矩阵，使其变为:
+ * [
+ *   [7,4,1],
+ *   [8,5,2],
+ *   [9,6,3]
+ * ]
+ * 示例 2:
+ * 给定 matrix =
+ * [
+ * 0, 0 -> 3, 3  0, 1 -> 2, 3
+ *   [ 5, 1, 9,11],
+ *   [ 2, 4, 8,10],
+ *   [13, 3, 6, 7],
+ *   [15,14,12,16]
+ * ],
+ * 原地旋转输入矩阵，使其变为:
+ * [
+ *   [15,13, 2, 5],
+ *   [14, 3, 4, 1],
+ *   [12, 6, 8, 9],
+ *   [16, 7,10,11]
+ * ]
+ */
+public class A0048 {
+    public static void main(String[] args) {
+//        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] matrix = {{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
+        rotate(matrix);
+    }
+    public static void rotate(int[][] matrix) {
+        int m = matrix.length;
+        if (m == 0) {
+            return;
+        }
+        for (int i = 0; i < m - 1; i++) {
+            for (int j = 0; j < m - 1 - i; j++) {
+                System.out.println(i + " " + j);
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[m - j - 1][m - i - 1];
+                matrix[m - j - 1][m - i - 1] = temp;
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int i = 0; i < m / 2; i++) {
+            for (int j = 0; j < m; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[m - 1 - i][j];
+                matrix[m - 1 - i][j] = temp;
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
